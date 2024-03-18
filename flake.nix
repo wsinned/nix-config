4c923f18d7616efd3f20a12a8db57f05c3de5ae4
wsinned <wsinned@users.noreply.github.com>
2024-03-18 10:55:21 +0000

    Update flake.nix
    Corrected typo in function

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
 
     # This is a function that generates an attribute by calling a function you
     # pass to it, with each system as an argument
-    forEachSystem = f: lib.genAttrs systems (system: f.pkgsFor.${system});
+    forEachSystem = f: lib.genAttrs systems (system: f pkgsFor.${system});
     pkgsFor = lib.genAttrs systems (system: import nixpkgs {
       inherit system;
       config.allowUnfree = true;
