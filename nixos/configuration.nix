9030d1df3e921314a8cb18a9e9bce50f8c6b60ba
Dennis Woodruff <denniswoodruffuk@gmail.com>
2024-03-20 17:55:33 +0000

    Removes common boot + hostname config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -15,12 +15,6 @@
       inputs.home-manager.nixosModules.home-manager
     ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  boot.initrd.luks.devices."luks-5ed9eb46-4414-4e23-a1db-626cc28aeb7f".device = "/dev/disk/by-uuid/5ed9eb46-4414-4e23-a1db-626cc28aeb7f";
-  networking.hostName = "dw-apollo"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
