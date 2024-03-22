089dab12ccf29b3d6d2b42dba9830aef733379b4
Dennis Woodruff <denniswoodruffuk@gmail.com>
2024-03-22 08:59:10 +0000

    Adds aliases and session vars to config.

---
+++ home-manager/config/aliases.nix
@@ -0,0 +1,28 @@
+home.shellAliases = {
+
+  
+  # misc aliases
+  ls='ls -F --color=auto --show-control-chars'
+  ll='ls -lh'
+  la='ls -alh'
+  v='nvim'
+  vs='code'
+
+  # git aliases
+  g='git'
+  s="git status -sbu"
+  l="git log --pretty=format:'%Cgreen%h%d %Creset%s %Cred%an, %Cgreen%ar%Creset' --graph"
+  ci="git commit"
+  ga="git add --all"
+
+
+  # nix aliases
+  nix-gens="nix profile history --profile /nix/var/nix/profiles/system"
+
+
+  thisWeek="take-note --thisWeek $args"
+  nextWeek="take-note --nextWeek $args"
+  lastWeek="take-note --lastWeek $args"
+  
+};
+
---
+++ home-manager/config/vars.nix
@@ -0,0 +1,4 @@
+home.sessionVariables = {
+  notes_folder="$HOME/Insync/denniswoodruffuk@gmail.com/Google\ Drive/Notes"
+  args="--notesFolder $notes_folder --workspace notes.code-workspace --template Home-weekly-log-template.md --batch 5"
+};
