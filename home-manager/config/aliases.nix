927875e1075793c85322045bb610d84a076194b9
Dennis Woodruff <denniswoodruffuk@gmail.com>
2024-03-22 10:14:17 +0000

    Corrects format of vars and aliases files.

--- home-manager/config/aliases.nix
+++ home-manager/config/aliases.nix
@@ -1,28 +1,29 @@
-home.shellAliases = {
-
+{
+  home.shellAliases = {
   
-  # misc aliases
-  ls='ls -F --color=auto --show-control-chars'
-  ll='ls -lh'
-  la='ls -alh'
-  v='nvim'
-  vs='code'
+    # misc aliases
+    ls = "ls -F --color=auto --show-control-chars";
+    ll = "ls -lh";
+    la = "ls -alh";
+    v = "nvim";
+    vs = "code";
 
-  # git aliases
-  g='git'
-  s="git status -sbu"
-  l="git log --pretty=format:'%Cgreen%h%d %Creset%s %Cred%an, %Cgreen%ar%Creset' --graph"
-  ci="git commit"
-  ga="git add --all"
-
-
-  # nix aliases
-  nix-gens="nix profile history --profile /nix/var/nix/profiles/system"
+    # git aliases
+    g = "git";
+    s = "git status -sbu";
+    l = "git log --pretty=format:'%Cgreen%h%d %Creset%s %Cred%an, %Cgreen%ar%Creset' --graph";
+    ci = "git commit";
+    ga = "git add --all";
 
 
-  thisWeek="take-note --thisWeek $args"
-  nextWeek="take-note --nextWeek $args"
-  lastWeek="take-note --lastWeek $args"
+    # nix aliases
+    nix-gens = "nix profile history --profile /nix/var/nix/profiles/system";
+
+
+    thisWeek = "take-note --thisWeek $args";
+    nextWeek = "take-note --nextWeek $args";
+    lastWeek = "take-note --lastWeek $args";
   
-};
+  };
+}
 
--- home-manager/config/vars.nix
+++ home-manager/config/vars.nix
@@ -1,4 +1,7 @@
-home.sessionVariables = {
-  notes_folder="$HOME/Insync/denniswoodruffuk@gmail.com/Google\ Drive/Notes"
-  args="--notesFolder $notes_folder --workspace notes.code-workspace --template Home-weekly-log-template.md --batch 5"
-};
+{
+  home.sessionVariables = {
+    # Vars for TakeNote
+    notes_folder = "$HOME/Insync/denniswoodruffuk@gmail.com/Google\ Drive/Notes";
+    args = "--notesFolder $notes_folder --workspace notes.code-workspace --template Home-weekly-log-template.md --batch 5";
+  };
+}
