961ba532cba0aff3d2701982c525fca687ef1df0
Dennis Woodruff <denniswoodruffuk@gmail.com>
2024-08-06 13:23:45 +0100

    Adds kitty launcher for vim

--- home-manager/apps/kitty.nix
+++ home-manager/apps/kitty.nix
@@ -4,5 +4,8 @@
     font.name = "MesloLGS NF";
     font.size = 14;
     shellIntegration.enableZshIntegration = true;
+    keybindings = {
+      "f1" =  "launch --cwd=current vi .";
+    };
   };
 }
