9ccf2d200a925c5846ee80f0b619d89160032896
Dennis Woodruff <denniswoodruffuk@gmail.com>
2024-08-07 10:10:45 +0100

    Enables tall layout in Kitty
    Also changes kitty new window custom shortcut to only open in cwd, without vi

--- home-manager/apps/kitty.nix
+++ home-manager/apps/kitty.nix
@@ -5,7 +5,10 @@
     font.size = 14;
     shellIntegration.enableZshIntegration = true;
     keybindings = {
-      "f1" =  "launch --cwd=current vi .";
+      "f1" =  "launch --cwd=current";
+    };
+    settings = {
+      enabled_layouts = "tall:bias=70;full_size=1;mirrored=false";
     };
   };
 }
