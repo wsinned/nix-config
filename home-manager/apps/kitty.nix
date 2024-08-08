cc0171b3a1bddc8d62f45cc6aab9cfc08f2bed21
Dennis Woodruff <denniswoodruffuk@gmail.com>
2024-08-08 10:01:28 +0100

    Adds stack layout for full screen edit
    Also styles the tab bar with powerline for clarity.

--- home-manager/apps/kitty.nix
+++ home-manager/apps/kitty.nix
@@ -8,7 +8,9 @@
       "f1" =  "launch --cwd=current";
     };
     settings = {
-      enabled_layouts = "tall:bias=70;full_size=1;mirrored=false";
+      enabled_layouts = "tall:bias=70;full_size=1;mirrored=false, stack";
+      tab_bar_style = "powerline";
+      tab_powerline_style = "angled";
     };
   };
 }
