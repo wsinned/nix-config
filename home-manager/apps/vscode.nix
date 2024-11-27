8e0d5112036e2ecc5219a9b14c9b4902db4d1d86
Dennis Woodruff <denniswoodruffuk@gmail.com>
2024-11-27 08:32:02 +0000

    Swaps VSCode to FHS version.

--- home-manager/apps/vscode.nix
+++ home-manager/apps/vscode.nix
@@ -3,17 +3,18 @@
 {
 	home.packages = with pkgs; [
 
-    (vscode-with-extensions.override { 
-      vscodeExtensions = with vscode-extensions; [
-        asvetliakov.vscode-neovim
-        rust-lang.rust-analyzer
-        ms-python.vscode-pylance
-        ms-python.python
-        ms-python.debugpy
-        charliermarsh.ruff
-        tamasfe.even-better-toml
-        denoland.vscode-deno
-      ];
-    })
+    vscode-fhs
+    # (vscode-with-extensions.override { 
+    #   vscodeExtensions = with vscode-extensions; [
+    #     asvetliakov.vscode-neovim
+    #     rust-lang.rust-analyzer
+    #     ms-python.vscode-pylance
+    #     ms-python.python
+    #     ms-python.debugpy
+    #     charliermarsh.ruff
+    #     tamasfe.even-better-toml
+    #     denoland.vscode-deno
+    #   ];
+    # })
 	];
 }
