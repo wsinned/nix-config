1599c25effd04f74555d3c184c3c7b8ee859c572
Dennis Woodruff <denniswoodruffuk@gmail.com>
2025-01-16 10:28:06 +0000

    Convert deprecated setting to new key.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -113,7 +113,7 @@
   };
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
