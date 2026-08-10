7e4707ccab0452a5fce36b02d63d2391bccdbccc
Dennis Woodruff <denniswoodruffuk@gmail.com>
2026-08-10 16:52:11 +0100

    Update flake.lock after updates.

--- hosts/dw-apollo/hardware-configuration.nix
+++ hosts/dw-apollo/hardware-configuration.nix
@@ -26,7 +26,10 @@
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices = [ ];
+    zramSwap = {
+      enable = true;
+      memoryPercent = 25;
+    };
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
@@ -67,4 +70,8 @@
   # environment.variables = {
   #   VDPAU_DRIVER = "va_gl";
   # };
+
+  services.fstrim.enable = true;
+  services.thermald.enable = true;
+  services.power-profiles-daemon.enable = true;
 }
