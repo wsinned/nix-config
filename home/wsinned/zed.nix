86b9aa5e2c25749d8a291eaea9b93f000dd62759
Dennis Woodruff <denniswoodruffuk@gmail.com>
2026-08-18 08:15:57 +0100

    Adds rust support to Zed

--- home/wsinned/zed.nix
+++ home/wsinned/zed.nix
@@ -6,6 +6,8 @@
 
     extensions = [
       "nix"
+      "rust"
+      "toml"
     ];
 
     extraPackages = with pkgs; [
@@ -14,18 +16,38 @@
     ];
 
     userSettings = {
+      vim_mode = true;
+      relative_line_numbers = "enabled";
+
       languages.Nix = {
         language_servers = [
           "nixd"
           "!nil"
+          "rust"
         ];
         formatter = "language_server";
         format_on_save = "on";
       };
 
-      lsp.nixd = {
-        binary.path_lookup = true;
-        settings.nixd.formatting.command = [ "nixfmt" ];
+      lsp = {
+        rust-analyzer = {
+          binary = {
+            # path = lib.getExe pkgs.rust-analyzer;
+            path_lookup = true;
+          };
+        };
+
+        nix = {
+          binary = {
+            path_lookup = true;
+          };
+        };
+
+        nixd = {
+          binary.path_lookup = true;
+          settings.nixd.formatting.command = [ "nixfmt" ];
+        };
+
       };
     };
   };
